de57046a447e1c8e0438fcea6c8d52a6058d77e1
Orri <orrmann@gmail.com>
2023-08-24 10:43:30 +0300

    Initial Commit

---
+++ KupkakeKorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KupkakeKorner
+//
+//  Created by Orri Arnórsson on 24.8.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
